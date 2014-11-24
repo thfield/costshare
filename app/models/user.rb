2a14b058674f55f04ec52a9f30c31296db48d371
thfield <thfield@gmail.com>
2014-11-24 13:03:23 -0800

    before branching

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ActiveRecord::Base
   									length: { maximum: 255 }, 
   									format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-	validates :password, length: { minimum:6 }             
+
 
   has_secure_password
-
+	validates :password, length: { minimum:6 }
 end
